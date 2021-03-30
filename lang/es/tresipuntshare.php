8b7091197a146f65e936ec7c839bc465555d75b6
Antonio Manzano <amanzano@tresipunt.com>
2021-03-30 11:55:36 +0200

    create project

---
+++ lang/es/tresipuntshare.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_tresipuntshare
+ * @category    string
+ * @copyright   2021 Tresipunt
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Compartir comentario';
+$string['modulename'] = 'Compartir comentario';
+$string['modulenameplural'] = 'Compartir comentarios';
+$string['missingidandcmid'] = 'Módulo del curso no encontrado';
+$string['comment'] = 'Comentario';
+$string['comment_help'] = 'Escribe un comentario para publicar en el aula.';
+$string['settings'] = 'Ajustes';
+$string['fieldset'] = 'Conjunto de campos';
+$string['pluginadministration'] = 'Administración compartir comentario';
---
+++ mod_form.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * The main mod_tresipuntshare configuration form.
+ *
+ * @package     mod_tresipuntshare
+ * @copyright   2021 Tresipunt
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+
+/**
+ * Module instance settings form.
+ *
+ * @package     mod_tresipuntshare
+ * @copyright   2021 Tresipunt
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_tresipuntshare_mod_form extends moodleform_mod {
+
+    /**
+     * Defines forms elements
+     *
+     * @throws coding_exception
+     */
+    public function definition() {
+        global $CFG, $USER;
+
+        $mform = $this->_form;
+
+        // Adding the "general" fieldset, where all the common settings are shown.
+        $mform->addElement('header', 'general', get_string('general', 'form'));
+
+        // Adding the standard "name" field.
+        $mform->addElement('text', 'name', get_string(
+            'comment', 'mod_tresipuntshare'), array('size' => '64'));
+
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('name', PARAM_TEXT);
+        } else {
+            $mform->setType('name', PARAM_CLEANHTML);
+        }
+
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addRule('name', get_string(
+            'maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addHelpButton(
+            'name', 'comment', 'mod_tresipuntshare');
+
+        $mform->addElement('hidden', 'teacher', $USER->id);
+        $mform->setType('teacher', PARAM_INT);
+
+        // Add standard elements.
+        $this->standard_coursemodule_elements();
+
+        // Add standard buttons.
+        $this->add_action_buttons();
+    }
+}
